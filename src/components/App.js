fcc7d37c50a36f580ca36bb51e7ce963402c5436
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-12-28 18:06:49 +0530

    code modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,26 @@
+//App.js
+// App.js
+import React from 'react';
+import Tooltip from './Tooltip';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Tooltip text="This is a Tooltip">
+        <h1 className="tooltip-title">Hover over me</h1>
+      </Tooltip>
+      <hr style={{ marginBottom: "50px" }} className="tooltip-divider" />
+      <Tooltip text="This is another tooltip">
+        <p className="tooltip-title">Hover over this paragraph</p>
+      </Tooltip>
+      <hr style={{ marginTop: "30px" }} className="tooltip-divider" />
+    </div>
+  );
+};
+
+export default App;
+
+/*
 import React from 'react';
 import Tooltip from './Tooltip';
 
@@ -17,3 +40,4 @@ const App = () => {
 };
 
 export default App;
+*/
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,3 +1,37 @@
+
+//Tooltip.js
+// Tooltip.js
+import React, { useState } from 'react';
+import '../styles/Tooltip.css';
+
+const Tooltip = ({ text, children }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
+  return (
+    <div 
+      className="tooltip" 
+      onMouseEnter={handleMouseEnter} 
+      onMouseLeave={handleMouseLeave}
+    >
+      {children}
+      {/* Render the tooltip conditionally based on the state */}
+      {showTooltip && <span className="tooltiptext">{text}</span>}
+    </div>
+  );
+};
+
+export default Tooltip;
+
+
+/*
 import React, { useState } from 'react';
 import '../styles/Tooltip.css';
 
@@ -21,3 +55,4 @@ const Tooltip = ({ text, children }) => {
 };
 
 export default Tooltip;
+*/
