64bbf5b7bde6f1b760e9ccbb0710c6c688f933e3
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-12-29 10:07:15 +0530

    code added

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -11,6 +11,30 @@ const Tooltip = ({ text, children }) => {
 
   return (
     <div
+      className="tooltip"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="tooltip-content">{children}</div>
+      {isTooltipVisible && <div className="tooltiptext">{text}</div>}
+    </div>
+  );
+};
+
+export default Tooltip;
+
+/*
+import React, { useState } from 'react';
+import '../styles/Tooltip.css';
+
+const Tooltip = ({ text, children }) => {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
+  const handleMouseEnter = () => setIsTooltipVisible(true);
+  const handleMouseLeave = () => setIsTooltipVisible(false);
+
+  return (
+    <div
       className="tooltip-wrapper"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
